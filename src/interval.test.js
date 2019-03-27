de3334c1698e6cdafa1b34bab832c69100ea04ae
omartalibi <omar.talibi.96@gmail.com>
2019-03-27 22:33:53 +0100

    Excludes tests

--- src/interval.test.js
+++ src/interval.test.js
@@ -32,19 +32,19 @@ it('overlaps/when_its_interval1_smallerThan_interval2', function () {
 
 it("includes/when_interval1_biggerThan_interval2", function(){
     let interval1 = new Interval(1,10)
-    let interval2 = new Interval(2,9)
+    let interval2 = new Interval(3,7)
     expect(interval1.includes(interval2)).toBe(true);
 })
 
 it("includes/when_interval1_sameStart_interval2", function(){
-    let interval1 = new Interval(2,9)
-    let interval2 = new Interval(2,10)
+    let interval1 = new Interval(2,6)
+    let interval2 = new Interval(2,7)
     expect(interval1.includes(interval2)).toBe(false);
 })
 
-it("includes/when_interval1_sameEnd_interval2", function(){
-    let interval1 = new Interval(1,10)
-    let interval2 = new Interval(1,9)
+it("includes/when_interval1_sameStart_interval2", function(){
+    let interval1 = new Interval(1,5)
+    let interval2 = new Interval(1,3)
     expect(interval1.includes(interval2)).toBe(true);
 })
 
@@ -52,4 +52,30 @@ it("includes/when_interval1_Equals_interval2", function(){
     let interval1 = new Interval(1,10)
     let interval2 = new Interval(1,10)
     expect(interval1.includes(interval2)).toBe(true);
+})
+
+//======
+
+it("includes/when_interval1_BiggerThan_interval2", function(){
+    let interval1 = new Interval(1,20)
+    let interval2 = new Interval(4,9)
+    expect(interval1.union(interval2)).toEqual([1,20]);
+})
+
+it("includes/when_interval1_inside_interval2", function(){
+    let interval1 = new Interval(4,7)
+    let interval2 = new Interval(1,8)
+    expect(interval1.union(interval2)).toEqual([1,8]);
+})
+
+it("includes/when_interval1_partiallyInside_interval2", function(){
+    let interval1 = new Interval(1,6)
+    let interval2 = new Interval(2,10)
+    expect(interval1.union(interval2)).toEqual([1,10]);
+})
+
+it("includes/when_interval2_partiallyInside_interval1", function(){
+    let interval1 = new Interval(2,7)
+    let interval2 = new Interval(1,6)
+    expect(interval1.union(interval2)).toEqual([1,7]);
 })
